1275999c309c79f7565d843a861142a228da51c5
Juro Zaw <jurozaw@gmail.com>
2025-12-30 22:58:38 -0500

    Hover

--- app/reload/components/menu-text.tsx
+++ app/reload/components/menu-text.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 import { Roboto } from "next/font/google";
 import { useState } from "react";
 
@@ -10,6 +10,7 @@ const roboto = Roboto({
 
 export default function MenuText({ children }: { children: React.ReactNode }) {
     const [width, setWidth] = useState<number | undefined>(undefined);
+    const [isHovered, setIsHovered] = useState(false);
     
     // Find the width of the hover background by the left position of the div
     const measureRef = (node: HTMLDivElement | null) => {
@@ -23,12 +24,17 @@ export default function MenuText({ children }: { children: React.ReactNode }) {
         <motion.div 
             ref={measureRef} 
             className="relative"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
             >
             <h1 className={`text-5xl uppercase tracking-tighter leading-8 ${roboto.className}`}>{children}</h1>
-            <motion.div 
-                className="h-8 z-[-1] bg-[#0101C7] absolute left-0 top-1"
-                style={{ width: width ?? '100%' }}
-            />
+            {isHovered && (
+                <motion.div 
+                    className="h-8 z-[-1] bg-[#0101C7] absolute left-0 top-1"
+                    style={{ width: width ?? '100%' }}
+                    layout
+                />
+            )}
         </motion.div>
     );
 }
--- app/reload/page.tsx
+++ app/reload/page.tsx
@@ -1,12 +1,15 @@
+import { LayoutGroup } from "framer-motion";
 import MenuText from "./components/menu-text";
 
 export default function ReloadPage() {
     return (
         
-        <div className="flex flex-col items-end">
-            <MenuText>Home</MenuText>
-            <MenuText>Components</MenuText>
-            <MenuText>Contact</MenuText>
+        <div className="flex flex-col items-end gap-2">
+            <LayoutGroup>
+                <MenuText>Home</MenuText>
+                <MenuText>Components</MenuText>
+                <MenuText>Contact</MenuText>
+            </LayoutGroup>
         </div>
     )
 }
