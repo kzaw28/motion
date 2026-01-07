0e28090cb60894506f013063a473e7659bd4f490
Juro Zaw <jurozaw@gmail.com>
2026-01-06 21:32:27 -0500

    Hover

--- app/reload/components/menu-text.tsx
+++ app/reload/components/menu-text.tsx
@@ -9,8 +9,8 @@ const roboto = Roboto({
 });
 
 export default function MenuText({ children }: { children: React.ReactNode }) {
-    const [width, setWidth] = useState<number | undefined>(undefined);
     const [isHovered, setIsHovered] = useState(false);
+    const [width, setWidth] = useState<number | undefined>(undefined);
     
     // Find the width of the hover background by the left position of the div
     const measureRef = (node: HTMLDivElement | null) => {
@@ -27,14 +27,27 @@ export default function MenuText({ children }: { children: React.ReactNode }) {
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             >
-            <h1 className={`text-5xl uppercase tracking-tighter leading-8 ${roboto.className}`}>{children}</h1>
-            {isHovered && (
-                <motion.div 
-                    className="h-8 z-[-1] bg-[#0101C7] absolute left-0 top-1"
-                    style={{ width: width ?? '100%' }}
-                    layout
-                />
-            )}
+            <motion.h1 
+                className={`text-5xl uppercase tracking-tighter leading-8 ${roboto.className}`}
+                initial={{ opacity: 0, y: 60 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.2 }}
+            >
+                {children}
+            </motion.h1>
+            <AnimatePresence>
+                {isHovered && (
+                    <motion.div 
+                        layoutId="hover-indicator"
+                        className="h-8 z-[-1] bg-[#0101C7] absolute left-0 top-1"
+                        style={{ width: width ?? '100%' }}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                    />
+                )}
+            </AnimatePresence>
         </motion.div>
     );
 }
--- app/reload/page.tsx
+++ app/reload/page.tsx
@@ -1,15 +1,13 @@
-import { LayoutGroup } from "framer-motion";
+"use client";
 import MenuText from "./components/menu-text";
 
 export default function ReloadPage() {
     return (
         
         <div className="flex flex-col items-end gap-2">
-            <LayoutGroup>
-                <MenuText>Home</MenuText>
-                <MenuText>Components</MenuText>
-                <MenuText>Contact</MenuText>
-            </LayoutGroup>
+            <MenuText>Home</MenuText>
+            <MenuText>Components</MenuText>
+            <MenuText>Contact</MenuText>
         </div>
     )
 }
